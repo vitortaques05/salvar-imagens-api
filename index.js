07ccae4695e694df793839bcaeeb88b34a3f5894
05632101096 <05632101096@senacrs.edu.br>
2024-04-08 21:27:24 -0300

    Aaa

--- index.js
+++ index.js
@@ -1,80 +1,70 @@
-const express = require('express')
-const cors = require('cors')
-const fs = require('fs')
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
 
-const dados = require('./dados.json')
+const dados = require('./dados.json');
 
-const server = express()
-server.use(express.json())
-server.use(cors())
+const server = express();
+server.use(express.json());
+server.use(cors());
 
 server.listen(3000, () => {
     console.log('O servidor está funcionando');
 });
 
-server.get('/Imagens', (req, res) => {
-    return res.json(dados.Imagens)
+server.get('/imagens', (req, res) => {
+    return res.json(dados.Imagens);
 });
 
-
-//SELECT * FROM CLIENTES
-
-//Create da API
+// Create da API
 server.post('/imagens', (req, res) => {
-    const novaImagem = req.body
+    const novaImagem = req.body;
 
-
-    if (!novaImagem.link || !novaImagem.url || !novaImagem.descricao) {
+    if (!novaImagem.url || !novaImagem.descricao) {
         return res.status(400).json({ mensagem: "Dados incompletos, tente novamente" });
     } else {
         // Adicionar a nova imagem aos dados existentes.
-        dadosImagens.push(novaImagem);
+        dados.Imagens.push(novaImagem);
         // Salvar os dados atualizados.
-        salvarDadosImagens(dadosImagens);
+        salvarDados();
     
         return res.status(201).json({ mensagem: "Dados completos, imagem cadastrada com sucesso" });
     }
-})
+});
 
-//Read da API
-server.get('/imagens', (req, res) => {
-    return res.json(dados.Imagens)
-})
-
-//Funcao que salva os dados
-function salvarDadosImagens() {
-    fs.writeFileSync(__dirname + '/dados.json', JSON.stringify(dados, null, 2))
-}
-
-//Update da API
+// Update da API
 server.put('/imagens/:id', (req, res) => {
-    const id = parseInt(req.params.id)
-    const atualizarImagens = req.body
+    const id = parseInt(req.params.id);
+    const atualizarImagem = req.body;
 
-    const indiceImagem = dados.Imagem.findIndex(c => c.id === id)
+    const indiceImagem = dados.Imagens.findIndex(img => img.id === id);
 
     if (indiceImagem === -1) {
-        return res.status(404).json({ mensagem: "Cliente não encontrado" })
+        return res.status(404).json({ mensagem: "Imagem não encontrada" });
     } else {
-        dados.Imagens[indiceImagem].nome = atualizarClient.link || dados.Imagems[indiceImagem].link
-        dados.Imagens[indiceImagem].url = atualizarClient.url || dados.Imagems[indiceImagem].url
-        dados.Imagens[indiceImagem].descricao = atualizarClient.email || dados.Imagems[indiceImagem].descricao
+       
+        dados.Imagens[indiceImagem].url = atualizarImagem.url || dados.Imagens[indiceImagem].url;
+        dados.Imagens[indiceImagem].descricao = atualizarImagem.descricao || dados.Imagens[indiceImagem].descricao;
 
-        salvarDados(dados)
+        salvarDados();
 
-        return res.status(201).json({ mensagem: "Dados completo, dados atualizados com sucesso" })
+        return res.status(201).json({ mensagem: "Dados completos, imagem atualizada com sucesso" });
     }
-})
+});
 
 // Delete da API
 server.delete('/imagens/:id', (req, res) => {
-    const id = parseInt(req.params.id)
+    const id = parseInt(req.params.id);
 
-    //filtrar as imagens, removendo pelo id correspondente
+    // Filtrar as imagens, removendo pelo id correspondente
+    dados.Imagens = dados.Imagens.filter(img => img.id !== id);
 
-    dados.Imagens = dados.Imagens.filter(c => c.id !== id)
+    salvarDados();
 
-    salvarDados(dados)
+    return res.status(200).json({ mensagem: "Imagem excluída com sucesso" });
+});
 
-    return res.status(200).json({ mensagem: "Imagem excluída com sucesso" })
-})
+// Função que salva os dados
+function salvarDados() {
+    fs.writeFileSync(__dirname + '/dados.json', JSON.stringify(dados, null, 2));
+}
